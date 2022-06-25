2d84873c6805d01a1cb6c4d3f94c7fb052fd9da0
Paul <euclyd@gmaiil.com>
2022-06-25 20:41:32 +0100

    day 35 commit

--- Day 35/main-35.py
+++ Day 35/main-35.py
@@ -1,10 +1,13 @@
 import requests
+import smtplib
 
 MY_LAT = 53.424150
 MY_LONG = -2.175040
 API_KEY = "4f3ac6ddd48d5bcde102213c02a7bf27"
 API_URL = "https://api.openweathermap.org/data/2.5/onecall"
-
+MY_EMAIL = "euclyd@gmail.com"
+MY_PASSWORD = "uzwpjqdzgfpzsnxy"
+TO = "euclyd@gmail.com"
 parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
@@ -26,10 +29,11 @@ for n in range(11):
    weather_id.append(data["hourly"][n]["weather"][0]["id"])
 
 if min(weather_id) < 700:
-    print("Bring an umbrella")
-
-#add twilio account for SMS update or SMTP for email ans scedule to run via PythonAnywhere every day
-
+    #add twilio account for SMS update or SMTP for email and schedule to run via PythonAnywhere every day
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO, msg=f"Subject: Rain forecast today\n\nDon't forget an umbrella!")
 
 
 
