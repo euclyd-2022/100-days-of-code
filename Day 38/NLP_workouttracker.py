1d01d1dc0658c0d99d821a5895b541b4eec7a92c
Paul <euclyd@gmail.com>
2022-07-03 11:20:52 +0100

    day 38 commit

---
+++ Day 38/NLP_workouttracker.py
@@ -0,0 +1,48 @@
+import requests
+from datetime import datetime
+
+APP_ID = "f54d07b4"
+APP_KEY = "2359558bf3495e4328426459883c4beb"
+ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+header = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+}
+params = {
+    "query": input("what exercise did you do?")
+}
+
+response = requests.post(url=ENDPOINT, json=params, headers=header)
+print(response.text)
+
+
+SHEETY_ENDPOINT = "https://api.sheety.co/2bd35d5df20d0a8d28cdd1935ca84bd2/myWorkouts/workouts"
+
+# example post {
+#   "email": {
+# 	"name": "Syed K",
+# 	"email": "syed@gmail.com"
+#   }
+# }
+
+today = datetime.now()
+date = today.strftime("%d/%m/%Y")
+time = today.strftime("%X")
+data = response.json()["exercises"]
+
+token = {
+"Authorization": "Bearer alpha751bcd"
+}
+sheety_json = {
+    "workout": {
+    "date": date,
+    "time": time,
+    "exercise": data[0]["name"].title(),
+    "duration": data[0]["duration_min"],
+    "calories": data[0]["nf_calories"]
+    }
+}
+
+sheety_post = requests.post(url=SHEETY_ENDPOINT, json=sheety_json, headers=token)
+print(sheety_post.text)
