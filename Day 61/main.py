1b05c11350334c8cf8f2c16b0b2008751ee9e03b
Paul <euclyd@gmail.com>
2022-07-27 08:39:35 +0100

    day 61 commit

--- Day 61/main.py
+++ Day 61/main.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+import email_validator
+
+
+class MyForm(FlaskForm):
+    email = StringField(label='email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='password', validators=[DataRequired(), Length(min=8)])
+    bob = PasswordField(label='bob', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In")
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'therandomstring'
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
