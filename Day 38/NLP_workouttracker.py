bfb2889cb84f6003a27ab701eeb6802a0a5dc727
Paul <euclyd@gmail.com>
2022-09-14 09:27:24 +0100

    day 70 commit

--- Day 38/NLP_workouttracker.py
+++ Day 38/NLP_workouttracker.py
@@ -1,13 +1,13 @@
 import requests
 from datetime import datetime
+from secrets import NLP_TOKEN,NLP_APP_ID,NLP_APP_KEY
 
-APP_ID = "f54d07b4"
-APP_KEY = "2359558bf3495e4328426459883c4beb"
+
 ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 header = {
-    "x-app-id": APP_ID,
-    "x-app-key": APP_KEY,
+    "x-app-id": NLP_APP_ID,
+    "x-app-key": NLP_APP_KEY,
 }
 params = {
     "query": input("what exercise did you do?")
@@ -32,7 +32,7 @@ time = today.strftime("%X")
 data = response.json()["exercises"]
 
 token = {
-"Authorization": "Bearer alpha751bcd"
+"Authorization": NLP_TOKEN
 }
 sheety_json = {
     "workout": {
